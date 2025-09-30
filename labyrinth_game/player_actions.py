160ff1f20a9a8b54c4ff78a34a3299caa2601223
Roman G <gusar.roman.a@gmail.com>
2025-10-01 01:39:44 +0300

    feat(player_actions): get input func

--- labyrinth_game/player_actions.py
+++ labyrinth_game/player_actions.py
@@ -6,3 +6,10 @@ def show_inventory(game_state: dict):
         print("Ваш инвентарь:", ', '.join(inventory))
     else:
         print("Кажется, ваш инвентарь пуст.")
+
+def get_input(prompt="> "):
+  try:
+      print(prompt, end='', flush=True)
+  except (KeyboardInterrupt, EOFError):
+      print("\nВыход из игры.")
+      return "quit" 
