bd78689570572759a6b5995da638be64a5420c38
Roman G <gusar.roman.a@gmail.com>
2025-10-01 02:09:38 +0300

    wip(main): process_command for user inputs

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,4 +1,51 @@
 #!/usr/bin/env python3
 
+from . import utils
+from . import player_actions
+
+
+game_state = {
+      'player_inventory': [], # Инвентарь игрока
+      'current_room': 'entrance', # Текущая комната
+      'game_over': False, # Значения окончания игры
+      'steps_taken': 0 # Количество шагов
+}
+
+def process_command(game_state: dict, command: str):
+    action, *args = command.strip().lower().split()
+    arg = args[0] if args else None
+
+    match action:
+        case "go":
+            player_actions.move_player(game_state, direction = arg)
+        case "inventory":
+            player_actions.show_inventory(game_state)
+        case "look":
+            utils.describe_current_room(game_state)
+        case "take":
+            player_actions.take_item(game_state, item_name = arg)
+        case "use":
+            pass
+            # player_actions.use_item(game_state, item_name = arg)
+        case "solve":
+            pass
+            # player_actions.solve_puzzle(game_state)
+        case "help":
+            utils.show_help()
+        case "quit" | "exit":
+            game_state['game_over'] = True
+            print("Спасибо за игру!")
+        case _:
+            print("Неизвестная команда. ")
+            utils.show_help()
+
 def main():
-  print("Первая попытка запустить проект! ")
+  print("Добро пожаловать в Лабиринт сокровищ!")
+  utils.describe_current_room(game_state)
+
+  while not game_state['game_over']:
+    input = player_actions.get_input()
+    process_command(game_state, input)
+
+if __name__ == "__main__":
+    main()
